6c19d8c8072dd693f2132b6211eb7d33f54aa80c
ajinorisenpai <thunderlove1111@gmail.com>
2019-08-27 21:21:30 +0900

    edit a little

--- src/Player.cpp
+++ src/Player.cpp
@@ -5,8 +5,173 @@
 //  Created by 坂井創一 on 2019/08/10.
 //
 
+#include <algorithm>
 #include "Player.hpp"
 #include "Game.hpp"
 void Player::shot_bullet(){
 GetGame()->make_bullet(center().x,center().y); 
 }
+
+void Player::handle_input(){
+    //攻撃ボタン
+    if(KeyC.pressed()){
+        shot_bullet();
+    }
+    
+    //ジャンプボタン
+    if(KeyX.pressed()&&p_st!=State::Jump){
+        p_st = State::Jump;
+        effect.add<Burn>(Vec2(pos.x+size.x/2,pos.y+size.y));
+        velocity.y = -jump_power;
+        pos.y -= 1;
+    }
+    
+    //移動ボタン+Zダッシュ
+    switch(p_st){
+        case Idle:
+            if(velocity.x > 0) velocity.x -=1;
+                if(velocity.x < 0) velocity.x +=1;
+                    if(KeyLeft.pressed()){
+                        if(facing_left){
+                            p_st = State::Walk;
+                            frame = 0;
+                        }else{
+                            p_st = State::Idle;
+                            facing_left = !facing_left;
+                            pos.x -=10;
+                            frame = 0;
+                        }
+                    }else
+                        if(KeyRight.pressed()){
+                            if(!facing_left){
+                                p_st = State::Walk;
+                                frame = 0;
+                            }else{
+                                p_st = State::Idle;
+                                facing_left = !facing_left;
+                                pos.x +=10;
+                                frame = 0;
+                            }
+                        }
+            break;
+        case Walk:
+            if(KeyLeft.pressed()){
+                if(facing_left){
+                    if(KeyZ.pressed()){
+                        p_st = State::Run;
+                        frame = 0;
+                        velocity.x = std::max<double>(-walkspeed_limit,velocity.x-1);
+                    }else{
+                        velocity.x = std::max<double>(-walkspeed_limit,velocity.x-1);
+                    }
+                }else{
+                    p_st = State::Idle;
+                    facing_left = !facing_left;
+                    frame = 0;
+                }
+            }else
+                if(KeyRight.pressed()){
+                    if(!facing_left){
+                        if(KeyZ.pressed()){
+                            p_st = State::Run;
+                            frame = 0;
+                            if(velocity.x < walkspeed_limit) velocity.x+=1;
+                                else velocity.x = walkspeed_limit;
+                                    }else{
+                                        if(velocity.x < walkspeed_limit) velocity.x+=1;
+                                            else velocity.x = walkspeed_limit;
+                                                }
+                    }else{
+                        p_st = State::Idle;
+                        facing_left = !facing_left;
+                        frame = 0;
+                    }
+                }else{
+                    p_st = State::Idle;
+                }
+            break;
+        case Run:
+            if(KeyLeft.pressed()){
+                if(facing_left){
+                    if(KeyZ.pressed()){
+                        if(velocity.x > -Runspeed_limit) velocity.x-=2;
+                            }else{
+                                p_st = State::Walk;
+                                if(velocity.x > -Runspeed_limit) velocity.x-=2;
+                                    }
+                }else{
+                    p_st = State::Idle;
+                    facing_left = !facing_left;
+                    frame = 0;
+                }
+            }else
+                if(KeyRight.pressed()){
+                    if(!facing_left){
+                        if(KeyZ.pressed()){
+                            if(velocity.x < Runspeed_limit) velocity.x+=2;
+                                }else{
+                                    p_st = State::Walk;
+                                    if(velocity.x < Runspeed_limit) velocity.x+=2;
+                                        }
+                    }else{
+                        p_st = State::Idle;
+                        facing_left = !facing_left;
+                        frame = 0;
+                    }
+                }else{
+                    p_st = State::Idle;
+                }
+            break;
+        case Jump:
+            velocity.y+=1;
+            if(pos.y>=500) {
+                pos.y = 500;
+                velocity.y = 0;
+                p_st = State::Idle;
+            }
+            if(KeyLeft.pressed()){
+                if(facing_left){
+                    if(velocity.x > -5) velocity.x-=1;
+                        else velocity.x=-5;
+                            }else{
+                                facing_left = !facing_left;
+                            }
+            }else if(KeyRight.pressed()){
+                if(!facing_left){
+                    if(velocity.x < 5) velocity.x+=1;
+                        else velocity.x=5;
+                            }else{
+                                facing_left = !facing_left;
+                            }
+            }
+            break;
+        case Turn:
+            if((frame/10) > anime_turn.size()-1){
+                p_st = State::Walk;
+                facing_left = !facing_left;
+            }
+            break;
+    }
+    
+}
+
+const void Player::draw() const{
+    switch(p_st){
+        case Idle:
+            texture_idle(72*(anime_idle[(frame/10) % anime_idle.size()]),0,72,72).mirrored(facing_left).draw(pos.x,pos.y);
+            break;
+        case Walk:
+            texture_walk(72*(anime_walk[(frame/10) % anime_walk.size()]),0,72,72).mirrored(facing_left).draw(pos.x,pos.y);
+            break;
+        case Turn:
+            texture_turn(72*(anime_turn[(frame/10) % anime_turn.size()]),0,72,72).mirrored(facing_left).draw(pos.x,pos.y);
+            break;
+        case Run:
+            texture_run(72*(anime_run[(frame/6) % anime_run.size()]),0,72,72).mirrored(facing_left).draw(pos.x,pos.y);
+            break;
+        case Jump:
+            texture_jump(72*(anime_jump[(frame/10) % anime_jump.size()]),0,72,72).mirrored(facing_left).draw(pos.x,pos.y);
+            break;
+    }
+//    HitBox.drawFrame();
+};
--- src/Player.hpp
+++ src/Player.hpp
@@ -50,27 +50,21 @@ public:
         texture_walk = Texture(U"Image/Unitychan/Unitychan_Walk.png");
         texture_idle = Texture(U"Image/Unitychan/Unitychan_Idle.png");
         texture_turn = Texture(U"Image/Unitychan/Unitychan_Turn.png");
-        
         texture_run = Texture(U"Image/Unitychan/Unitychan_Run.png");
         texture_jump = Texture(U"Image/Unitychan/Unitychan_Jump.png");
     }
     bool collision(Circle& c){
         return HitBox.intersects(c);
-        
     }
-    
-    s3d::Vec2 center(){
+    Vec2 center(){
         return HitBox.center();
     }
     
     void update(){
         pos += velocity;
-        HitBox = Rect(pos.x,pos.y, size.x,size.y);
         handle_input();
         frame+=1;
         effect.update();
-    }
-    void handle_input(){
         
         //壁との衝突
         if(pos.x<0){
@@ -82,169 +76,15 @@ public:
             pos.x = Scene::Width()-size.x;
         }
         
-        //攻撃ボタン
-        if(KeyC.pressed()){
-            shot_bullet();
-        }
         
-        //ジャンプボタン
-        if(KeyX.pressed()&&p_st!=State::Jump){
-            p_st = State::Jump;
-            effect.add<Burn>(Vec2(pos.x+size.x/2,pos.y+size.y));
-            velocity.y = -jump_power;
-            pos.y -= 1;
-        }
-        
-        //移動ボタン+Zダッシュ
-        switch(p_st){
-            case Idle:
-                if(velocity.x > 0) velocity.x -=1;
-                if(velocity.x < 0) velocity.x +=1;
-                if(KeyLeft.pressed()){
-                    if(facing_left){
-                        p_st = State::Walk;
-                        frame = 0;
-                    }else{
-                        p_st = State::Idle;
-                        facing_left = !facing_left;
-                        frame = 0;
-                    }
-                }else
-                if(KeyRight.pressed()){
-                    if(!facing_left){
-                        p_st = State::Walk;
-                        frame = 0;
-                    }else{
-                        p_st = State::Idle;
-                        facing_left = !facing_left;
-                        frame = 0;
-                    }
-                }
-                break;
-            case Walk:
-                if(KeyLeft.pressed()){
-                    if(facing_left){
-                        if(KeyZ.pressed()){
-                            p_st = State::Run;
-                            frame = 0;
-                            if(velocity.x > -walkspeed_limit) velocity.x-=1;
-                            else velocity.x = -walkspeed_limit;
-                        }else{
-                            if(velocity.x > -walkspeed_limit) velocity.x-=1;
-                            else velocity.x = -walkspeed_limit;
-                        }
-                    }else{
-                        p_st = State::Idle;
-                        facing_left = !facing_left;
-                        frame = 0;
-                    }
-                }else
-                if(KeyRight.pressed()){
-                    if(!facing_left){
-                        if(KeyZ.pressed()){
-                            p_st = State::Run;
-                            frame = 0;
-                            if(velocity.x < walkspeed_limit) velocity.x+=1;
-                            else velocity.x = walkspeed_limit;
-                        }else{
-                           if(velocity.x < walkspeed_limit) velocity.x+=1;
-                            else velocity.x = walkspeed_limit;
-                        }
-                    }else{
-                        p_st = State::Idle;
-                        facing_left = !facing_left;
-                        frame = 0;
-                    }
-                }else{
-                    p_st = State::Idle;
-                }
-                break;
-            case Run:
-                if(KeyLeft.pressed()){
-                    if(facing_left){
-                        if(KeyZ.pressed()){
-                            if(velocity.x > -Runspeed_limit) velocity.x-=2;
-                        }else{
-                            p_st = State::Walk;
-                            if(velocity.x > -Runspeed_limit) velocity.x-=2;
-                        }
-                    }else{
-                        p_st = State::Idle;
-                        facing_left = !facing_left;
-                        frame = 0;
-                    }
-                }else
-                    if(KeyRight.pressed()){
-                        if(!facing_left){
-                            if(KeyZ.pressed()){
-                                if(velocity.x < Runspeed_limit) velocity.x+=2;
-                            }else{
-                                p_st = State::Walk;
-                                if(velocity.x < Runspeed_limit) velocity.x+=2;
-                            }
-                        }else{
-                            p_st = State::Idle;
-                            facing_left = !facing_left;
-                            frame = 0;
-                        }
-                    }else{
-                        p_st = State::Idle;
-                    }
-                break;
-            case Jump:
-                velocity.y+=1;
-                if(pos.y>=500) {
-                    pos.y = 500;
-                    velocity.y = 0;
-                    p_st = State::Idle;
-                }
-                if(KeyLeft.pressed()){
-                    if(facing_left){
-                        if(velocity.x > -5) velocity.x-=1;
-                        else velocity.x=-5;
-                    }else{
-                        facing_left = !facing_left;
-                    }
-                }else if(KeyRight.pressed()){
-                    if(!facing_left){
-                        if(velocity.x < 5) velocity.x+=1;
-                        else velocity.x=5;
-                    }else{
-                        facing_left = !facing_left;
-                    }
-                }
-                break;
-            case Turn:
-                if((frame/10) > anime_turn.size()-1){
-                    p_st = State::Walk;
-                    facing_left = !facing_left;
-                }
-                break;
-        }
+        HitBox = Rect(pos.x,pos.y, size.x,size.y);
         
     }
+    void handle_input();
     
     void shot_bullet();
     //メンバ関数の右側にconstをつけると、そのメンバ関数内ではメンバ変数の変更ができなくなる
-    const void draw() const{
-        switch(p_st){
-            case Idle:
-                texture_idle(72*(anime_idle[(frame/10) % anime_idle.size()]),0,72,72).mirrored(facing_left).draw(pos.x,pos.y);
-                break;
-            case Walk:
-                texture_walk(72*(anime_walk[(frame/10) % anime_walk.size()]),0,72,72).mirrored(facing_left).draw(pos.x,pos.y);
-                break;
-            case Turn:
-                texture_turn(72*(anime_turn[(frame/10) % anime_turn.size()]),0,72,72).mirrored(facing_left).draw(pos.x,pos.y);
-                break;
-            case Run:
-                texture_run(72*(anime_run[(frame/6) % anime_run.size()]),0,72,72).mirrored(facing_left).draw(pos.x,pos.y);
-                break;
-            case Jump:
-                texture_jump(72*(anime_jump[(frame/10) % anime_jump.size()]),0,72,72).mirrored(facing_left).draw(pos.x,pos.y);
-                break;
-        }
-    };
+    const void draw() const;
     
 };
 
